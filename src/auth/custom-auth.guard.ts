9390bb51bf00a08a16c8eca5ce3a830772a70441
daejae <daejae135@daum.net>
2024-06-05 12:28:13 +0900

    add : prisma 모듈, 가드처리

---
+++ src/auth/custom-auth.guard.ts
@@ -0,0 +1,44 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CustomAuthGuard implements CanActivate {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['api-key'];
+
+    console.log(authHeader);
+    if (!authHeader) {
+      throw new UnauthorizedException('Authorization api-key is missing');
+    }
+
+    const user = await this.validateToken(authHeader);
+    if (!user) {
+      throw new UnauthorizedException('Invalid api-key');
+    }
+    request.user = user;
+
+    return true;
+  }
+
+  async validateToken(token: string): Promise<any> {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        apiKey: token,
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+}
