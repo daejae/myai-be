a361467e9a1bb88abc66d8abc8095d4646ffaf84
daejae <daejae135@daum.net>
2024-06-07 07:55:21 +0900

    add : class-validator, class-transformer module 추가

--- src/web/web.controller.spec.ts
+++ src/web/web.controller.spec.ts
@@ -3,7 +3,7 @@ import { WebController } from './web.controller';
 import { WebService } from './web.service';
 import { OpenaiModule } from 'src/openai/openai.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { EnvModule } from 'src/config/config.module';
+import { EnvModule } from 'src/env/env.module';
 
 describe('WebController', () => {
   let controller: WebController;
