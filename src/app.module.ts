6378f6202fae7806fb2a443eb91ccbe084e8f597
daejae <daejae135@daum.net>
2024-06-06 16:06:12 +0900

    change : config 모듈 이름 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { WebModule } from './web/web.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { EnvModule } from './config/config.module';
+import { EnvModule } from './env/env.module';
 
 @Module({
   imports: [EnvModule, WebModule, PrismaModule],
--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigModule } from '@nestjs/config';
-import configuration from 'src/config/configuration';
+import configuration from 'src/env/configuration';
 
 export async function createTestingModule(): Promise<TestingModule> {
   return Test.createTestingModule({
