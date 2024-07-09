c0089f87921a232de1c70511e6a9b250f80fc83a
daejae <daejae135@daum.net>
2024-07-09 13:05:19 +0900

    Feature : gpt api(텍스트 생성) 로직 retry 추가

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -22,131 +22,149 @@ export class OpenaiService {
     prompt,
     assistantId,
     threadId,
+    retries = 3,
   }: {
     prompt: string;
     assistantId: string;
     threadId: string;
+    retries: number;
   }) {
-    const list = [];
-    const extractJson = (str: string): object | null => {
-      const startIndex = str.indexOf('{'); // JSON 시작 지점 찾기
-      if (startIndex === -1) return null; // 시작 괄호가 없으면 null 반환
+    try {
+      const list: Array<{ title: string; story: string; next: boolean }> = [];
+      const extractJson = (str: string): object | null => {
+        const startIndex = str.indexOf('{'); // JSON 시작 지점 찾기
+        if (startIndex === -1) return null; // 시작 괄호가 없으면 null 반환
 
-      let counter = 1; // 중괄호 카운터 시작
-      for (let i = startIndex + 1; i < str.length; i++) {
-        if (str[i] === '{') {
-          counter++;
-        } else if (str[i] === '}') {
-          counter--;
-          if (counter === 0) {
-            // const plain = str.substring(startIndex, i + 1);
-            // const fix = plain
-            //   .replace(/}\s*{/g, '},{')
-            //   .replace(/(?<=["\]}])\s*(?=["\[{])/g, ',')
-            //   .replace(/(\r\n|\n|\r)/g, '') // 모든 개햄문자 삭제
-            //   .replace(/\\'/g, "'"); // 홀따옴표 처리
-            // // return jsonString.replace(/}\s*{/g, '},{').replace(/(?<=["\]}])\s*(?=["\[{])/g, ',');
-            // return JSON.parse(fix);
+        let counter = 1; // 중괄호 카운터 시작
+        for (let i = startIndex + 1; i < str.length; i++) {
+          if (str[i] === '{') {
+            counter++;
+          } else if (str[i] === '}') {
+            counter--;
+            if (counter === 0) {
+              // const plain = str.substring(startIndex, i + 1);
+              // const fix = plain
+              //   .replace(/}\s*{/g, '},{')
+              //   .replace(/(?<=["\]}])\s*(?=["\[{])/g, ',')
+              //   .replace(/(\r\n|\n|\r)/g, '') // 모든 개햄문자 삭제
+              //   .replace(/\\'/g, "'"); // 홀따옴표 처리
+              // // return jsonString.replace(/}\s*{/g, '},{').replace(/(?<=["\]}])\s*(?=["\[{])/g, ',');
+              // return JSON.parse(fix);
 
-            const plain = str.substring(startIndex, i + 1);
-            const fixedPlain = plain
-              .replace(/}\s*{/g, '},{') // 중첩된 JSON 객체를 배열 형식으로 변환
-              .replace(/(?<=["\]}])\s*(?=["\[{])/g, ',') // 객체 사이에 쉼표 추가
-              .replace(/(\r\n|\n|\r|\\n)/g, ' ') // 모든 개행 문자 삭제
-              .replace(/\\'/g, "'") // 이스케이프된 작은따옴표 처리
-              .replace(/\\\\"/g, `\\"`);
+              const plain = str.substring(startIndex, i + 1);
+              const fixedPlain = plain
+                .replace(/}\s*{/g, '},{') // 중첩된 JSON 객체를 배열 형식으로 변환
+                .replace(/(?<=["\]}])\s*(?=["\[{])/g, ',') // 객체 사이에 쉼표 추가
+                .replace(/(\r\n|\n|\r|\\n)/g, ' ') // 모든 개행 문자 삭제
+                .replace(/\\'/g, "'") // 이스케이프된 작은따옴표 처리
+                .replace(/\\\\"/g, `\\"`);
 
-            try {
-              return JSON.parse(fixedPlain);
-            } catch (e) {
-              console.error('JSON 파싱 오류:', e);
-              console.log('파싱 시도한 문자열:', fixedPlain);
-              throw e;
+              try {
+                return JSON.parse(fixedPlain);
+              } catch (e) {
+                console.error('JSON 파싱 오류:', e);
+                console.log('파싱 시도한 문자열:', fixedPlain);
+                throw e;
+              }
             }
           }
         }
-      }
-      return null;
-    };
+        return null;
+      };
 
-    // 초기 메시지 생성
-    const initialMessage = await this.openai.beta.threads.messages.create(
-      threadId,
-      {
-        role: 'user',
-        content: prompt,
-      },
-    );
-
-    const run = this.openai.beta.threads.runs.stream(threadId, {
-      assistant_id: assistantId,
-    });
-
-    for await (const event of run) {
-      if (event.event == 'thread.message.completed') {
-        const result = (event.data.content[0] as any)?.text.value;
-        console.log(result);
-
-        if (result.length < 0)
-          throw new HttpException(
-            'GPT API error',
-            HttpStatus.INTERNAL_SERVER_ERROR,
-          );
-        const jsonData = extractJson(result) as {
-          title: string;
-          story: string;
-          next: boolean;
-        };
-        if (!jsonData) {
-          throw new HttpException(
-            'No valid JSON found or parsing error',
-            HttpStatus.INTERNAL_SERVER_ERROR,
-          );
-        }
-        list.push(jsonData);
-      }
-    }
-
-    if (list[list.length - 1].next == true) {
-      while (true) {
-        await this.openai.beta.threads.messages.create(threadId, {
+      // 초기 메시지 생성
+      const initialMessage = await this.openai.beta.threads.messages.create(
+        threadId,
+        {
           role: 'user',
-          content: '계속 이어서',
-        });
+          content: prompt,
+        },
+      );
 
-        const run = this.openai.beta.threads.runs.stream(threadId, {
-          assistant_id: assistantId,
-        });
+      const run = this.openai.beta.threads.runs.stream(threadId, {
+        assistant_id: assistantId,
+      });
 
-        let result = '';
-        for await (const event of run) {
-          if (event.event == 'thread.message.completed') {
-            result = (event.data.content[0] as any)?.text.value;
-            console.log(result);
+      for await (const event of run) {
+        if (event.event == 'thread.message.completed') {
+          const result = (event.data.content[0] as any)?.text.value;
+          console.log(result);
+
+          if (result.length < 0)
+            throw new HttpException(
+              'GPT API error',
+              HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+          const jsonData = extractJson(result) as {
+            title: string;
+            story: string;
+            next: boolean;
+          };
+          if (!jsonData) {
+            throw new HttpException(
+              'No valid JSON found or parsing error',
+              HttpStatus.INTERNAL_SERVER_ERROR,
+            );
           }
+          list.push(jsonData);
         }
+      }
 
-        if (result.length < 0)
-          throw new HttpException(
-            'GPT API error',
-            HttpStatus.INTERNAL_SERVER_ERROR,
-          );
+      if (list[list.length - 1].next == true) {
+        while (true) {
+          await this.openai.beta.threads.messages.create(threadId, {
+            role: 'user',
+            content: '계속 이어서',
+          });
 
-        const jsonData = extractJson(result) as {
-          title: string;
-          story: string;
-          next: boolean;
-        };
-        list.push(jsonData);
+          const run = this.openai.beta.threads.runs.stream(threadId, {
+            assistant_id: assistantId,
+          });
 
-        if (jsonData.next == false) break;
+          let result = '';
+          for await (const event of run) {
+            if (event.event == 'thread.message.completed') {
+              result = (event.data.content[0] as any)?.text.value;
+              console.log(result);
+            }
+          }
+
+          if (result.length < 0)
+            throw new HttpException(
+              'GPT API error',
+              HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+
+          const jsonData = extractJson(result) as {
+            title: string;
+            story: string;
+            next: boolean;
+          };
+          list.push(jsonData);
+
+          if (jsonData.next == false) break;
+        }
+      }
+
+      return {
+        title: list[0].title,
+        story: list.map((item) => item.story).join(' '),
+      };
+    } catch (error) {
+      if (retries > 0) {
+        console.log(`Retrying... (${3 - retries + 1})`);
+        return await this.generateTextWithAssistant({
+          assistantId,
+          threadId,
+          prompt,
+          retries: retries - 1,
+        });
+      } else {
+        throw new Error(
+          `Failed to fetch data after 3 retries: ${error.message}`,
+        );
       }
     }
-
-    return {
-      title: list[0].title,
-      story: list.map((item) => item.story).join(' '),
-    };
   }
 
   async generateThumbnail({
