0b119a2083bd461b5f9edd9441bd25f3cfc44af8
daejae <daejae135@daum.net>
2024-07-19 20:12:13 +0900

    Fix : 빌드결과물 경로변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "app", // 애플리케이션 이름
-      script: "dist/main.js", // 실행할 스크립트
+      script: "dist/src/main.js", // 실행할 스크립트
       instances: 1, // 인스턴스 수 (0이면 모든 CPU 코어 사용)
       exec_mode: "fork", // 실행 모드: 'fork' 또는 'cluster'
       watch: false, // 파일 변경 시 자동 재시작
