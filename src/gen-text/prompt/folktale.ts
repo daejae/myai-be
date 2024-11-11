5ffb1ea3a176c6581591a8843e6fd22ee0a409dc
demo <demo>
2024-11-11 14:18:33 +0900

    Feature : 한국동화 관련 한국 도시들 추가

--- src/gen-text/prompt/folktale.ts
+++ src/gen-text/prompt/folktale.ts
@@ -26,6 +26,109 @@ const countries = [
   '필리핀',
 ];
 
+const cities_KR = [
+  '서울',
+  '부산',
+  '대구',
+  '인천',
+  '광주',
+  '대전',
+  '울산',
+  '수원',
+  '전주',
+  '춘천',
+  '강릉',
+  '안동',
+  '경주',
+  '제주',
+  '목포',
+  '포항',
+  '청주',
+  '천안',
+  '성남',
+  '의정부',
+  '김해',
+  '마산',
+  '진주',
+  '여수',
+  '순천',
+  '군산',
+  '익산',
+  '정읍',
+  '남원',
+  '나주',
+  '통영',
+  '거제',
+  '사천',
+  '창원',
+  '밀양',
+  '양산',
+  '충주',
+  '제천',
+  '속초',
+  '동해',
+  '삼척',
+  '태백',
+  '영주',
+  '문경',
+  '상주',
+  '구미',
+  '김천',
+  '영천',
+  '영덕',
+  '의성',
+  '울진',
+  '보령',
+  '서산',
+  '태안',
+  '공주',
+  '부여',
+  '논산',
+  '아산',
+  '예산',
+  '홍성',
+  '강진',
+  '해남',
+  '장흥',
+  '진도',
+  '완도',
+  '고흥',
+  '광양',
+  '담양',
+  '화순',
+  '장성',
+  '함평',
+  '영암',
+  '무안',
+  '신안',
+  '부안',
+  '고창',
+  '임실',
+  '순창',
+  '진안',
+  '무주',
+  '장수',
+  '금산',
+  '영동',
+  '옥천',
+  '괴산',
+  '증평',
+  '음성',
+  '단양',
+  '철원',
+  '화천',
+  '양구',
+  '인제',
+  '고성',
+  '평창',
+  '영월',
+  '정선',
+  '횡성',
+  '홍천',
+  '양평',
+  '가평',
+];
+
 // 국가 코드를 국가 이름으로 매핑하는 객체
 const countryMap: { [key: string]: string } = {
   Folktale_KR: '한국',
@@ -51,9 +154,15 @@ export const getLongFolktale = async (
   const country = getCountryName(category)
     ? getCountryName(category)
     : getRandomElement(countries);
+  const userPrompt =
+    category == 'Folktale_KR'
+      ? `${country}의 ${getRandomElement(
+          cities_KR,
+        )}지역 동화를 썰로 매우매우 길게 작성해줘. title과 story로 JSON 객체로 반환해줘.`
+      : `${country}의 동화를 썰로 매우매우 길게 작성해줘. title과 story로 JSON 객체로 반환해줘.`;
 
   const draft = await openai.createChat({
-    userPrompt: `${country}의 동화를 썰로 매우매우 길게 작성해줘. title과 story로 JSON 객체로 반환해줘.`,
+    userPrompt,
     model: 'gpt-4o',
     isJson: true,
   });
@@ -83,8 +192,15 @@ export const getShortFolktale = async (
     ? getCountryName(category)
     : getRandomElement(countries);
 
+  const userPrompt =
+    category == 'Folktale_KR'
+      ? `${country}의 ${getRandomElement(
+          cities_KR,
+        )}지역 동화를 썰로 매우매우 길게 작성해줘. title과 story로 JSON 객체로 반환해줘.`
+      : `${country}의 동화를 썰로 매우매우 길게 작성해줘. title과 story로 JSON 객체로 반환해줘.`;
+
   const draft = await openai.createChat({
-    userPrompt: `${country}의 동화를 썰로 매우매우 길게 작성해줘. title과 story로 JSON 객체로 반환해줘.`,
+    userPrompt,
     model: 'gpt-4o',
     isJson: true,
   });
